fbb2f3bb476e5e8d111265cfce5b79efb2ec90cf
Nuno Francisco Azevedo Gomes <9180580@esmad.ipp.pt>
2019-01-09 15:12:00 +0000

    h

--- Projeto C/PaginaPrincipal.Designer.cs
+++ Projeto C/PaginaPrincipal.Designer.cs
@@ -85,7 +85,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(155, 90);
             this.button3.TabIndex = 5;
-            this.button3.Text = "Notificações";
+            this.button3.Text = "Enviar notificações";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
